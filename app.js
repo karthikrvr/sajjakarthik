93e1f1827d526ba60ec9c9ed5e3ad9f2e3e33f02
karthikrvr <karthik.sajja@gmail.com>
2017-09-21 13:16:34 +0530

    new feature completed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function aBigNewFeature() {
    console.log('Writing a lot of code.');
    console.log('Halfway done with this new feature');
+   console.log('Finally completely done with tis new feature.');
  };
 function something(){
 	console.log('I\'m something');
